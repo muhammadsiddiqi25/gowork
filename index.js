bd20ad58c5bc45926ee2e95b0926cd7d14065174
muhammadsiddiqi25 <116882764+muhammadsiddiqi25@users.noreply.github.com>
2023-02-21 00:41:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 app.get('*', (req, resp) => {
-    resp.send(path.resolve(__dirname,'client','build','index.html'))
+    resp.send('index.html')
 })
 
 
